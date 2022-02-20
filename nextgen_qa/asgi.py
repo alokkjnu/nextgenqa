1213f3613909224157dcb6fb3ee442e35c8a4270
alokkjnu <ry7069577@gmail.com>
2022-02-20 22:33:17 +0530

    update

---
+++ nextgen_qa/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for nextgen_qa project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nextgen_qa.settings')
+
+application = get_asgi_application()
